6deac07772c901fedb7f6fa2cc6602e6411ba019
Nikhil Anand <112142828+Nikhilmvk@users.noreply.github.com>
2026-01-29 16:22:25 +0530

    Update container name and external port

--- terraform/container.tf
+++ terraform/container.tf
@@ -1,10 +1,10 @@
 resource "docker_container" "webapp" {
-  name  = "webapp_v2"
+  name  = "webapp_v3"
   image = docker_image.webapp.image_id
 
   ports {
     internal = 80
-    external = 8073
+    external = 8072
   }
 
   restart = "always"
